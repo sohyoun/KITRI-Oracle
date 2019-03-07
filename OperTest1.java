55a01f13a364def001e76c77f2836c17e8f8199b
sohyoun <47847847+sohyoun@users.noreply.github.com>
2019-03-07 16:35:15 +0900

    Add files via upload

---
+++ OperTest1.java
@@ -0,0 +1,16 @@
+public class OperTest1 
+{
+	public static void main(String[] args) 
+	{
+		int x = 10;
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(--x);
+		System.out.println(x++);
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+		System.out.println(x++);
+		System.out.println(x);
+	}
+}
